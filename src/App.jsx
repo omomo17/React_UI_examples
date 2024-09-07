dc840178cd07843b8d6f3b44a86c28752befd4cf
momo <omomo061017@gmail.com>
2024-09-07 23:54:47 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-9xl tracking-tight text-transparent">My App</h1>
+    </div>
+  );
+}
+
+export default App;
