079a917addbd1becea36bee518c852cdc5b1f575
momo <omomo061017@gmail.com>
2024-09-16 23:10:30 +0900

    window manager updated by O1 preview v3 is bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 // import App_ from './SideBarNavigation/components/Sidebar';
 // import App_ from './MiniWindow/WindowManager';
-import App_ from './MiniWindow/WindowManagerO1';
+import App_ from './MiniWindow/WindowManagerO1_v3';
 // import App_ from './3Dportfolio/App';
 
 const App = () => {
