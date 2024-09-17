2176946cd4dfae47b319d9ae21db4bd5baf98156
momo <omomo061017@gmail.com>
2024-09-17 23:13:48 +0900

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import App_ from './SideBarNavigation/components/Sidebar';
 // import App_ from './MiniWindow/WindowManager';
 // import App_ from './MiniWindow/WindowManagerO1_v4';
-import App_ from './MiniWindow2/App';
+import App_ from './MiniWindow3/App';
 // import App_ from './3Dportfolio/App';
 
 const App = () => {
---
+++ src/MiniWindow3/App.jsx
@@ -0,0 +1,11 @@
+import WindowManager from './WindowManager';
+
+function App() {
+    return (
+        <div className="App">
+            <WindowManager />
+        </div>
+    );
+}
+
+export default App;
