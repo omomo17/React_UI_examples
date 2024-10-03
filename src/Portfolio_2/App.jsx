d1755f6432386765ce5990864630641d62d52e66
omomo <omomo1017@gmail.com>
2024-10-03 16:56:43 +0900

    skew background bug fixed

--- src/Portfolio_2/App.jsx
+++ src/Portfolio_2/App.jsx
@@ -1,11 +1,11 @@
 const App = () => {
     return (
       <div className="relative">
-        <div className="h-screen bg-pastel-pink skew-y-12 relative z-50 -mt-96" />
-        <div className="h-screen bg-pastel-blue -skew-y-12 relative z-40 -mt-96" />
-        <div className="h-screen bg-pastel-green skew-y-12 relative z-30 -mt-96" />
-        <div className="h-screen bg-pastel-yellow -skew-y-12 relative z-20 -mt-96" />
-        <div className="h-screen bg-pastel-purple relative z-10 -mt-96" />
+        <div className="h-[1500px] bg-pastel-pink skew-y-12 relative z-50 -mt-[500px]"/>
+        <div className="h-[2500px] bg-pastel-blue -skew-y-12 relative z-40 -mt-[800px]" />
+        <div className="h-[2500px] bg-pastel-green skew-y-12 relative z-30 -mt-[800px]" />
+        <div className="h-[2500px] bg-pastel-yellow -skew-y-12 relative z-20 -mt-[800px]" />
+        <div className="h-[2500px] bg-pastel-purple relative z-10 -mt-[500px]" />
       </div>
     );
   };
@@ -14,6 +14,19 @@ const App = () => {
 
 
 
+//   const App = () => {
+//     return (
+//       <div className="relative">
+//         <div className="h-[1500px] bg-pastel-pink skew-y-12 relative z-50 -mt-[500px]"/>
+//         <div className="h-screen bg-pastel-blue -skew-y-12 relative z-40 -mt-96" />
+//         <div className="h-screen bg-pastel-green skew-y-12 relative z-30 -mt-96" />
+//         <div className="h-screen bg-pastel-yellow -skew-y-12 relative z-20 -mt-96" />
+//         <div className="h-screen bg-pastel-purple relative z-10 -mt-96" />
+//       </div>
+//     );
+//   };
+  
+//   export default App;
 
 
 
@@ -71,6 +84,46 @@ const App = () => {
 //           className="h-screen bg-pastel-purple"
 //           style={{ clipPath: 'polygon(0 0, 100% 0, 100% 85%, 0 100%)' }}
 //         />
+//       </div>
+//     );
+//   };
+  
+//   export default App;
+
+
+// const App = () => {
+//     return (
+//       <div className="relative">
+//         {/* セクション1 */}
+//         <div
+//           className="relative h-screen bg-pastel-pink"
+//           style={{
+//             clipPath: 'polygon(0 0, 100% 0, 100% 85%, 0 100%)',
+//           }}
+//         />
+//         {/* セクション2 */}
+//         <div
+//           className="relative h-screen bg-pastel-blue -mt-32"
+//           style={{
+//             clipPath: 'polygon(0 15%, 100% 0, 100% 100%, 0 100%)',
+//           }}
+//         />
+//         {/* セクション3 */}
+//         <div
+//           className="relative h-screen bg-pastel-green -mt-32"
+//           style={{
+//             clipPath: 'polygon(0 0, 100% 0, 100% 85%, 0 100%)',
+//           }}
+//         />
+//         {/* セクション4 */}
+//         <div
+//           className="relative h-screen bg-pastel-yellow -mt-32"
+//           style={{
+//             clipPath: 'polygon(0 15%, 100% 0, 100% 100%, 0 100%)',
+//           }}
+//         />
+//         {/* セクション5 */}
+//         <div className="relative h-screen bg-pastel-purple -mt-32" />
 //       </div>
 //     );
 //   };
